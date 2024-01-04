5836eadeb7836c12e86e1f477b09a8bf6034e136
Alicia <admonty27@gmail.com>
2024-01-04 12:25:40 -0500

    Worked on NavBar

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ window.onload = function () {
 		const usa = utc + 3600000 * offset;
 
 		const time = new Date(usa).toLocaleTimeString();
-		const date = new Date(usa).toLocaleDateString();
 
-		document.getElementById("date").innerHTML = date + "&nbsp;";
 		document.getElementById("time").innerHTML = time;
 	}, 1000); // 1000 milliseconds = 1 second
 };
