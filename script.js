fa9f367d0e87dba43104df9b00c512959006ec91
Alicia <admonty27@gmail.com>
2024-01-24 03:07:08 -0500

    Completed Mobile Design

--- script.js
+++ script.js
@@ -14,11 +14,38 @@ window.onload = function () {
 	}, 1000); // 1000 milliseconds = 1 second
 };
 
+function toProjects() {
+	window.location.href = "projects.html";
+}
+
 function toResume() {
-	const name = document.querySelector("#projectsButton");
-	// add click event listener
-	name.addEventListener("click", () => {
-		// open a new tab
-		window.location.href = "projects.html";
-	});
+	// open a new tab
+	window.open("MontgomeryAlicia_Resume_Web.pdf", "_blank", "noopener");
+}
+
+function toGBCSWeb() {
+	const name = document.querySelector("#toGBCS");
+	// open a new tab
+	window.open("https://www.gbcsgroup.com/", "_blank", "noopener");
+}
+
+function toSpa() {
+	const name = document.querySelector("#toSpa");
+
+	// open a new tab
+	window.open(
+		"https://www.figma.com/file/1GAJ04hvYdnSnrLCw8wF9r/Spa-Website-Redesign---IT-649---AliciaMontgomery?type=design&node-id=105%3A359&mode=design&t=2oepzuc3MuZ4qZ37-1",
+		"_blank",
+		"noopener"
+	);
+}
+
+function display(id) {
+	var content = document.getElementById(id);
+
+	if (content.style.display !== "none") {
+		content.style.display = "none";
+	} else {
+		content.style.display = "block";
+	}
 }
