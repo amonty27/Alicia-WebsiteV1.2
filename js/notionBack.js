6e8e5a04502179012fb3d78c4a18814f741d1fc5
Alicia <admonty27@gmail.com>
2024-02-04 03:40:39 -0500

    Added Spotify and Notion
    - Added embeds for Spotify and Notion
    - organized scripts into folder

---
+++ js/notionBack.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#notionBack");
+
+var iframe = document.getElementById("iframe");
+
+btn.addEventListener("click", () => {
+	iframe.src = "https://www.notioniframe.com/notion/wnxm42o4wx";
+});
